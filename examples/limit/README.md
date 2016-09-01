9a9f1d416c0388e5bc3562e2d68b61d8e01808b4
Florent Dubois <florent.dubois@devaddict.io>
2016-09-01 22:41:13 +0200

    Add example for limited insertions

---
+++ examples/limit/README.md
@@ -0,0 +1,7 @@
+# Example: insert a limited number of times
+
+This example shows how to use the module `stream-insert` to innsert text a limited number of times.
+
+It inserts the line 'Line C' after the line 'Line B' only two times.
+
+See [the source](limit.js) for more information on the module usage.
---
+++ examples/limit/limit.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+
+var StreamInsert = require('../..');
+
+var input  = fs.createReadStream(path.join(__dirname, 'input.txt'));
+var output = fs.createWriteStream(path.join(__dirname, 'output.txt'));
+
+input.pipe(new StreamInsert('Line C', 'Line B', {limit: 2})).pipe(output);
